5707458dde1f7334244907d661e6dd002ec76ec7
alex g <alexgfotos@gmail.com>
2020-03-06 11:32:56 -0700

    removed silly JS and html and moved questions to an object array

--- script.js
+++ script.js
@@ -4,15 +4,76 @@
 //   });
 
 
-$( "#Q1").hide()
+$("#Q1").hide();
 
 
-
-$("#ready").click(function(){
-    $( "#Ready").slideUp();
-    $( "#Q1").show();
+$("#notReady").click(function () {
+    alert("Your training is complete, YOU ARE READY");
 })
 
-$("#notReady").click(function(){
-    alert("Your training is complete, YOU ARE READY");
+
+
+var questions = [
+    {
+        question: "CSS is ______ of a website's anthropromorphic analogy?",
+        answers: ["The Butt", "The Muscle", "The Epidermis", "The Medulla Oblongata"],
+        correct: 2
+    },
+    {
+        question: "JS is ______ of a website's anthropromorphic analogy?",
+        answers: ["The Butt", "The Muscle", "The Epidermis", "The Medulla Oblongata"],
+        correct: 1
+    },
+    {
+        question: "The term boolean has two possible values called ____ & ____.",
+        answers: ["True & false.", "Big & Small", "Gif & Gif", "Butt & Other Butt"],
+        correct: 0
+    },
+    {
+        question: "JS is ______ of a website's anthropromorphic analogy?",
+        answers: ["The Butt", "The Muscle", "The Epidermis", "The Medulla Oblongata"],
+        correct: 1
+    },
+    {
+        question: "JS is ______ of a website's anthropromorphic analogy?",
+        answers: ["The Butt", "The Muscle", "The Epidermis", "The Medulla Oblongata"],
+        correct: 1
+    },
+]
+
+
+var currentQuestion =0;
+var score =0;
+
+
+$("#ready").click(function () {
+    $("#Ready").slideUp();
+    $("#Q1").show();
+    display();
+    })
+
+function display(){
+    for ( i = 0 ; i < questions.length ; i++){
+        $(".question").text(questions[i].question);
+        $("#button1").text(questions[i].answers[0]);
+        $("#button2").text(questions[i].answers[1]);
+        $("#button3").text(questions[i].answers[2]);
+        $("#button4").text(questions[i].answers[3]);
+    }
+    // render the current question
+    // render the curret answers
+}
+
+$(".answers").on("click", function(){
+    
+   
+   
+   
+    // check if answer right/wrong
+    // if correct add one to score
+
+    // next questioc
+    currentQuestion++;
+    display();
+
 })
